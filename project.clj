4ebccfedaf69285e01a5f34faeda813086f01f47
Chetan Padia <chetan@swiftkey.com>
2015-02-15 12:55:31 +0000

    adding lein-midje to project

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje      "1.6.3"]]
+                   :plugins      [[lein-midje "3.1.3"]]}})
+
